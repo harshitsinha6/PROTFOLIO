52ca59b9b6d1dd28dae58b05276f67254c4581e8
Harshit Sinha <harshitsinha6@gmail.com>
2021-01-22 10:55:22 +0530

    'mycommit'

---
+++ profile-frontend/src/data/config.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  // TODO: change your profile information here
+  name: "Harshit Raj Sinha",
+  greeting: "Hello 👋",
+  greetingDescription: "I'm Harshit Raj Sinha and I'm a Student!",
+  githubUrl: "https://github.com/harshitsinha6",
+  linkedinUrl: "www.linkedin.com/in/harshit-raj-sinha-77922516a",
+  cvLink: "https://docs.google.com/document/d/1E1234561NMyQA67890ygda7abcdefghijs1jBp7HFI0/",
+};
