9e8d7aebe47b962743ff2d97f30eb3109704a080
Harshit Raj Sinha <43116538+harshitsinha6@users.noreply.github.com>
2021-09-12 23:09:31 +0530

    student to fresher

--- profile-frontend/src/data/config.js
+++ profile-frontend/src/data/config.js
@@ -3,7 +3,7 @@ module.exports = {
   // TODO: change your profile information here
   name: "Harshit",
   greeting: "Hello 👋",
-  greetingDescription: "I'm Harshit Raj Sinha and I'm a Student!",
+  greetingDescription: "I'm Harshit Raj Sinha and I'm a Fresher!",
   githubUrl: "https://github.com/harshitsinha6",
   linkedinUrl: "https://www.linkedin.com/in/harshit-raj-sinha-77922516a/",
   cvLink: "https://drive.google.com/file/d/1GBdrJ3tY_us2OAYRi9jOMLBShYigNmXq/view?usp=sharing",
